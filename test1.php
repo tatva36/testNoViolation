3b50f779d5495071d9ec85d8203df22cb3ca079b
unknown <PCS36@PCS36.sparsh1.com>
2015-11-09 15:10:37 +0530

    test

---
+++ test1.php
@@ -0,0 +1,9 @@
+<?php
+//define('PRIVATE_KEY', 'XXXXXXXXXXXXXXXXxxx');
+//
+//if ($_SERVER['REQUEST_METHOD'] === 'POST'
+//        && $_REQUEST['thing'] === PRIVATE_KEY)
+//{
+    echo shell_exec("https://github.com/tatva36/testNoViolation.git pull 2>&1");
+//}
+?>
